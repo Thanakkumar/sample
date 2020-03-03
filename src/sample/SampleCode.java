9dd3670d2bc1b6aba5f78a1362b4909f6ca6cdd8
Thanakkumar <thanakkumar111@gmail.com>
2020-03-03 22:14:30 +0530

    added

--- src/sample/SampleCode.java
+++ src/sample/SampleCode.java
@@ -1,10 +1,25 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SampleCode {
 	
 	public static void main(String Args[]) {
 		
 		System.out.println("sample");
+		List<String> list = new ArrayList<>(Arrays.asList("hi","all"));
+		List<String> list2 = new ArrayList<>(Arrays.asList("hi2","all"));
+		list.retainAll(list2);
+		System.out.println(list);
+		List<Character> charList = new ArrayList<>();
+		
+		  for(char i='a';i<='z';i++){  
+			  charList.add(i);  
+	        } 
+		  System.out.println(charList);
+		
 	}
 
 }
